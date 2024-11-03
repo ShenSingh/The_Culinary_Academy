38061872098dd0660172c6a114cb921aa0b1e744
Shen8Singh <gayashan9090@gmail.com>
2024-11-03 19:17:20 +0530

    create all ui

--- src/main/java/com/zenveus/the_culinary_academy/controllers/EmployeeController.java
+++ src/main/java/com/zenveus/the_culinary_academy/controllers/EmployeeController.java
@@ -1,7 +1,52 @@
 package com.zenveus.the_culinary_academy.controllers;
 
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
+import javafx.util.Duration;
 
-public class EmployeeController {
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmployeeController implements Initializable {
     public Text title;
+
+    public AnchorPane employeeRegMainAnchor;
+
+    public ImageView reportLeftRightImage;
+
+    private TranslateTransition sideTransition;
+    private boolean isShow = false;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setTransition();
+    }
+    private void setTransition() {
+        sideTransition = new TranslateTransition(Duration.seconds(1.5), employeeRegMainAnchor);
+        sideTransition.setFromX(0);
+        sideTransition.setToX(550); // Set initial `toX` based on `isShow`
+        updateIcon();
+    }
+
+    private void updateIcon() {
+        String iconPath = isShow
+                ? "src/main/resources/com/zenveus/the_culinary_academy/image/icons/rightArow.png"
+                : "src/main/resources/com/zenveus/the_culinary_academy/image/icons/leftArow.png";
+        Image image = new Image(new File(iconPath).toURI().toString());
+        reportLeftRightImage.setImage(image);
+    }
+
+    public void employeeAddPaneShowHideBtn(ActionEvent actionEvent) {
+        isShow = !isShow;
+        sideTransition.setDuration(Duration.seconds(isShow ? 1.5 : 2));
+        sideTransition.setToX(isShow ? 550 : 0);
+        updateIcon();
+        sideTransition.play();
+    }
 }
