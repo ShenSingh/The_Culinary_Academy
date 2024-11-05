94584b3f72c5b2dc4e3134509607eda9ca0576a7
Shen8Singh <gayashan9090@gmail.com>
2024-11-05 21:13:32 +0530

    done dashboard

--- src/main/java/com/zenveus/the_culinary_academy/controllers/DashboardController.java
+++ src/main/java/com/zenveus/the_culinary_academy/controllers/DashboardController.java
@@ -4,20 +4,89 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class DashboardController {
+public class DashboardController  implements Initializable {
 
 
     public AnchorPane mainContainer;
     public Text title;
+    public LineChart<String, Number> paymentLineChart;
+    public Text programCount;
+    public Text studentCount;
+    public Text codinatorCount;
+
+    public PieChart studentPieChart;
+    public PieChart programPieChart;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setChartData();
+    }
+
+    private void setChartData() {
+        // Create a data series for payments
+        XYChart.Series<String, Number> paymentSeries = new XYChart.Series<>();
+        paymentSeries.setName("Monthly Payments");
+
+        // Add demo data (Month and Payment Amount)
+        paymentSeries.getData().add(new XYChart.Data<>("January", 150));
+        paymentSeries.getData().add(new XYChart.Data<>("February", 200));
+        paymentSeries.getData().add(new XYChart.Data<>("March", 180));
+        paymentSeries.getData().add(new XYChart.Data<>("April", 220));
+        paymentSeries.getData().add(new XYChart.Data<>("May", 240));
+        paymentSeries.getData().add(new XYChart.Data<>("June", 300));
+        paymentSeries.getData().add(new XYChart.Data<>("July", 280));
+        paymentSeries.getData().add(new XYChart.Data<>("August", 310));
+        paymentSeries.getData().add(new XYChart.Data<>("September", 270));
+        paymentSeries.getData().add(new XYChart.Data<>("October", 290));
+        paymentSeries.getData().add(new XYChart.Data<>("November", 320));
+        paymentSeries.getData().add(new XYChart.Data<>("December", 330));
+
+        // Add the series to the chart
+        paymentLineChart.getData().add(paymentSeries);
+
+
+//        pie chart
+        // Add demo data to the PieChart
+        PieChart.Data courseA = new PieChart.Data("Course A", 30);
+        PieChart.Data courseB = new PieChart.Data("Course B", 25);
+        PieChart.Data courseC = new PieChart.Data("Course C", 20);
+        PieChart.Data courseD = new PieChart.Data("Course D", 15);
+        PieChart.Data courseE = new PieChart.Data("Course E", 10);
+
+        // Add data to the PieChart
+        studentPieChart.getData().addAll(courseA, courseB, courseC, courseD, courseE);
+
+
+
+
+        // Add demo data to the PieChart
+        PieChart.Data programA = new PieChart.Data("Culinary Arts", 40);
+        PieChart.Data programB = new PieChart.Data("Baking & Pastry", 25);
+        PieChart.Data programC = new PieChart.Data("Food Science", 15);
+        PieChart.Data programD = new PieChart.Data("Hospitality Management", 10);
+        PieChart.Data programE = new PieChart.Data("Nutrition", 10);
+
+        // Add data to the PieChart
+        programPieChart.getData().addAll(programA, programB, programC, programD, programE);
+
+
+    }
 
     //
     public void logOutBtn(ActionEvent actionEvent) {
@@ -146,4 +215,6 @@ public class DashboardController {
         Platform.exit();
         System.exit(0);
     }
+
+
 }
